5c454f887935e614818e6ca59759fd66fd1d1332
rajaborntohack <digisailordev@gmail.com>
2020-07-05 13:20:37 +0530

    as

--- public/assets/js/evolta-map.js
+++ public/assets/js/evolta-map.js
@@ -8,14 +8,14 @@
     window.initMap = function() {
         var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
-        center: {lat: 59.325, lng: 18.070}
+        center: {lat: 8.788779, lng: 78.1359641}
         });
 
         marker = new google.maps.Marker({
         map: map,
         draggable: true,
         animation: google.maps.Animation.DROP,
-        position: {lat: 59.327, lng: 18.067}
+        position: {lat: 8.788779, lng: 78.1359641}
         });
         marker.addListener('click', toggleBounce);
     }
